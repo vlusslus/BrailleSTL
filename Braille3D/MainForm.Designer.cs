9a476b19b6a337b9815c2b0708fbf1742da56e95
vlusslus <konstantinovr1@gmail.com>
2016-06-14 14:19:31 +0300

    Подклчение к БД работает.

--- Braille3D/MainForm.Designer.cs
+++ Braille3D/MainForm.Designer.cs
@@ -28,33 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxGlyphs = new System.Windows.Forms.GroupBox();
+            this.checkBoxEng = new System.Windows.Forms.CheckBox();
+            this.checkBoxRu = new System.Windows.Forms.CheckBox();
+            this.checkBoxNum = new System.Windows.Forms.CheckBox();
+            this.checkBoxSep = new System.Windows.Forms.CheckBox();
+            this.buttonSelectAll = new System.Windows.Forms.Button();
+            this.buttonDeselectAll = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBoxGlyphs.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // groupBoxGlyphs
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBoxGlyphs.Controls.Add(this.buttonDeselectAll);
+            this.groupBoxGlyphs.Controls.Add(this.buttonSelectAll);
+            this.groupBoxGlyphs.Controls.Add(this.checkBoxSep);
+            this.groupBoxGlyphs.Controls.Add(this.checkBoxNum);
+            this.groupBoxGlyphs.Controls.Add(this.checkBoxRu);
+            this.groupBoxGlyphs.Controls.Add(this.checkBoxEng);
+            this.groupBoxGlyphs.Location = new System.Drawing.Point(18, 18);
+            this.groupBoxGlyphs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBoxGlyphs.Name = "groupBoxGlyphs";
+            this.groupBoxGlyphs.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBoxGlyphs.Size = new System.Drawing.Size(202, 239);
+            this.groupBoxGlyphs.TabIndex = 0;
+            this.groupBoxGlyphs.TabStop = false;
+            this.groupBoxGlyphs.Text = "Глифы";
             // 
-            // Form1
+            // checkBoxEng
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.checkBoxEng.AutoSize = true;
+            this.checkBoxEng.Location = new System.Drawing.Point(8, 39);
+            this.checkBoxEng.Name = "checkBoxEng";
+            this.checkBoxEng.Size = new System.Drawing.Size(189, 24);
+            this.checkBoxEng.TabIndex = 0;
+            this.checkBoxEng.Text = "Англиский алфавит";
+            this.checkBoxEng.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRu
+            // 
+            this.checkBoxRu.AutoSize = true;
+            this.checkBoxRu.Location = new System.Drawing.Point(8, 70);
+            this.checkBoxRu.Name = "checkBoxRu";
+            this.checkBoxRu.Size = new System.Drawing.Size(168, 24);
+            this.checkBoxRu.TabIndex = 1;
+            this.checkBoxRu.Text = "Русский алфавит";
+            this.checkBoxRu.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNum
+            // 
+            this.checkBoxNum.AutoSize = true;
+            this.checkBoxNum.Location = new System.Drawing.Point(8, 101);
+            this.checkBoxNum.Name = "checkBoxNum";
+            this.checkBoxNum.Size = new System.Drawing.Size(91, 24);
+            this.checkBoxNum.TabIndex = 2;
+            this.checkBoxNum.Text = "Цифры";
+            this.checkBoxNum.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSep
+            // 
+            this.checkBoxSep.AutoSize = true;
+            this.checkBoxSep.Location = new System.Drawing.Point(8, 132);
+            this.checkBoxSep.Name = "checkBoxSep";
+            this.checkBoxSep.Size = new System.Drawing.Size(138, 24);
+            this.checkBoxSep.TabIndex = 3;
+            this.checkBoxSep.Text = "Разделители";
+            this.checkBoxSep.UseVisualStyleBackColor = true;
+            // 
+            // buttonSelectAll
+            // 
+            this.buttonSelectAll.Location = new System.Drawing.Point(7, 162);
+            this.buttonSelectAll.Name = "buttonSelectAll";
+            this.buttonSelectAll.Size = new System.Drawing.Size(188, 30);
+            this.buttonSelectAll.TabIndex = 4;
+            this.buttonSelectAll.Text = "Выбрать все";
+            this.buttonSelectAll.UseVisualStyleBackColor = true;
+            this.buttonSelectAll.Click += new System.EventHandler(this.buttonSelectAll_Click);
+            // 
+            // buttonDeselectAll
+            // 
+            this.buttonDeselectAll.Location = new System.Drawing.Point(8, 198);
+            this.buttonDeselectAll.Name = "buttonDeselectAll";
+            this.buttonDeselectAll.Size = new System.Drawing.Size(187, 31);
+            this.buttonDeselectAll.TabIndex = 5;
+            this.buttonDeselectAll.Text = "Сбросить";
+            this.buttonDeselectAll.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(363, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(357, 210);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(345, 179);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 315);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(807, 485);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBoxGlyphs);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Трансялтор шрифта Брайля";
+            this.groupBoxGlyphs.ResumeLayout(false);
+            this.groupBoxGlyphs.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxGlyphs;
+        private System.Windows.Forms.Button buttonDeselectAll;
+        private System.Windows.Forms.Button buttonSelectAll;
+        private System.Windows.Forms.CheckBox checkBoxSep;
+        private System.Windows.Forms.CheckBox checkBoxNum;
+        private System.Windows.Forms.CheckBox checkBoxRu;
+        private System.Windows.Forms.CheckBox checkBoxEng;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Braille3D/MainForm.cs
+++ Braille3D/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,40 @@ namespace Braille3D
             InitializeComponent();
         }
 
+        private void buttonSelectAll_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = Braille3D.Properties.Settings.Default.BrailleConnectionString;
+                string queryString = "SELECT * FROM Glyphs";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                try
+                {
+                    string text = "";
+                    while(reader.Read())
+                    {
+                        text += reader.GetString(1);
+                    }
+                    richTextBox1.Text = text;
+                }
+                catch (SqlException)
+                {
+                    
+                }
+                finally
+                {
+                    reader.Close();
+                }
+                //connection.Open();
+
+                Console.WriteLine("State: {0}", connection.State);
+                Console.WriteLine("ConnectionString: {0}",
+                    connection.ConnectionString);
+            }
+            //richTextBox1.Text = Braille3D.Properties.Settings.Default.BrailleConnectionString;
+        }
     }
 }
